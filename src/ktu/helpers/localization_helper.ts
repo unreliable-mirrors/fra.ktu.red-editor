800969c7680ad5669641c4e2a1c160bb484cbef1
Andres Marin <mb.andres@gmail.com>
2025-09-21 21:07:29 +0200

    Square Modulator (#111)

--- src/ktu/helpers/localization_helper.ts
+++ src/ktu/helpers/localization_helper.ts
@@ -24,6 +24,7 @@ const texts: Record<string, string> = {
   add_hue_offset_shader_button: "Hue Offset",
 
   add_sine_modulator_button: "Sine",
+  add_square_modulator_button: "Square",
 
   export_state: "Save File",
   import_state: "Import File",
